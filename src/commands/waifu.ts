5cd5122495493eae70a434d1097cb0674e3c51b4
SushiWaUmai <eugene.bb1@gmail.com>
2021-11-05 22:35:39 +0100

    feat(command): :sparkles: add waifu command

---
+++ src/commands/waifu.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { MessageEmbed } from "discord.js";
+import { Command } from "../types";
+
+interface WaifuResponse {
+  url: string;
+}
+
+const cmd: Command = {
+  name: "waifu",
+  description: "Sends a random waifu image",
+  execute: async (msg, _args) => {
+    const res = await axios.get("https://api.waifu.pics/sfw/waifu");
+    const data: WaifuResponse = res.data;
+    await msg.reply({
+      embeds: [new MessageEmbed().setImage(data.url).setColor("#ff00ff")],
+    });
+  },
+};
+
+module.exports = cmd;
