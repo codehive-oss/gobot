ac6e4ea2db6c99440a990d4b503cfea9f16351ca
Duckulus <duckelite1@gmail.com>
2021-11-05 21:37:39 +0100

    feat(db): added user db methods

---
+++ src/db/GoUser.ts
@@ -0,0 +1,29 @@
+import {BaseEntity, Column, Entity, PrimaryColumn} from "typeorm";
+import {User} from "discord.js";
+
+@Entity()
+export class GoUser extends BaseEntity {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    item: number[];
+
+    @Column()
+    balance: number
+}
+
+export async function exists(user: User) {
+    return await GoUser.findOne({where: {id: user.id}})
+}
+
+export async function create(user: User) {
+    const newUser = GoUser.create({
+        id: user.id,
+        balance: 0,
+        item: []
+    })
+
+    return await newUser.save()
+
+}
