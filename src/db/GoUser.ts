726294817e49389a1f289baf5905047e031ce44c
SushiWaUmai <eugene.bb1@gmail.com>
2021-11-05 21:50:28 +0100

    feat(db): :sparkles: add upsert in go user

--- src/db/GoUser.ts
+++ src/db/GoUser.ts
@@ -1,29 +1,37 @@
-import {BaseEntity, Column, Entity, PrimaryColumn} from "typeorm";
-import {User} from "discord.js";
+import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
+import { User } from "discord.js";
 
 @Entity()
 export class GoUser extends BaseEntity {
-    @PrimaryColumn()
-    id: string;
+  @PrimaryColumn()
+  id: string;
 
-    @Column()
-    item: number[];
+  @Column()
+  item: number[];
 
-    @Column()
-    balance: number
+  @Column()
+  balance: number;
 }
 
 export async function exists(user: User) {
-    return await GoUser.findOne({where: {id: user.id}})
+  return await GoUser.findOne({ where: { id: user.id } });
 }
 
 export async function create(user: User) {
-    const newUser = GoUser.create({
-        id: user.id,
-        balance: 0,
-        item: []
-    })
+  const newUser = GoUser.create({
+    id: user.id,
+    balance: 0,
+    item: [],
+  });
 
-    return await newUser.save()
-
+  return await newUser.save();
 }
+
+export const upsert = async (user: User) => {
+  const goUser = await exists(user);
+  if (goUser) {
+    return goUser;
+  } else {
+    return create(user);
+  }
+};
