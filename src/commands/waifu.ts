2b1d6dd93a0b6a22ec2509fc83acf1765a05d3c2
SushiWaUmai <eugene.bb1@gmail.com>
2021-11-06 21:55:32 +0100

    fix(command): :bug: resolve waifu command merge conflict

--- src/commands/waifu.ts
+++ src/commands/waifu.ts
@@ -7,26 +7,6 @@ interface WaifuResponse {
 }
 
 const cmd: Command = {
-<<<<<<< HEAD
-    aliases: [],
-    name: "waifu",
-    description: "Sends a random waifu image",
-    execute: async (msg, args) => {
-
-        if (args[0] === "nsfw") {
-            if (!(msg.channel as TextChannel).nsfw) {
-                await msg.reply("Yo you are not in a nsfw chat");
-                return;
-            }
-        }
-
-        const res = await axios.get(`https://api.waifu.pics/sfw/waifu`);
-        const data: WaifuResponse = res.data;
-        await msg.reply({
-            embeds: [new MessageEmbed().setImage(data.url).setColor("#ff00ff")],
-        });
-    }
-=======
   name: "waifu",
   description: "Sends a random waifu image",
   execute: async (msg, _args) => {
@@ -36,7 +16,6 @@ const cmd: Command = {
       embeds: [new MessageEmbed().setImage(data.url).setColor("#ff00ff")],
     });
   },
->>>>>>> 9bb01032b2f9e6081e7604ae4c019a13446743e0
 };
 
 module.exports = cmd;
