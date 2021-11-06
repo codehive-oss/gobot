b5fc397aaf451c44b056e943ea97b1db43abd4dd
SushiWaUmai <eugene.bb1@gmail.com>
2021-11-06 20:08:04 +0100

    revert: :fire: remove nsfw waifu command

--- src/commands/waifu.ts
+++ src/commands/waifu.ts
@@ -9,19 +9,8 @@ interface WaifuResponse {
 const cmd: Command = {
   name: "waifu",
   description: "Sends a random waifu image",
-  execute: async (msg, args) => {
-    let imageType = "sfw";
-
-    if (args[0] === "nsfw") {
-      if (!(msg.channel as TextChannel).nsfw) {
-        msg.reply("Yo you are not in a nsfw chat");
-        return;
-      }
-
-      imageType = "nsfw";
-    }
-
-    const res = await axios.get(`https://api.waifu.pics/${imageType}/waifu`);
+  execute: async (msg, _args) => {
+    const res = await axios.get(`https://api.waifu.pics/sfw/waifu`);
     const data: WaifuResponse = res.data;
     await msg.reply({
       embeds: [new MessageEmbed().setImage(data.url).setColor("#ff00ff")],
