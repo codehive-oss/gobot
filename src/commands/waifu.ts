de967b416518806cc8a52f8092ade824d49fab7b
Duckulus <duckelite1@gmail.com>
2021-11-07 17:14:24 +0100

    as

--- src/commands/waifu.ts
+++ src/commands/waifu.ts
@@ -1,28 +1,26 @@
 import axios from "axios";
-import { MessageEmbed } from "discord.js";
-import {canExecute, Command, CooldownCommand, setCooldown} from "../utils/types";
+import {MessageEmbed} from "discord.js";
+import {CooldownCommand} from "../utils/types";
 
 interface WaifuResponse {
-  url: string;
+    url: string;
 }
 
+//TODO add cooldown
 const cmd: CooldownCommand = {
-  name: "waifu",
-  description: "Sends a random waifu image",
-  cooldown: 200,
-  execute: async (msg, _args) => {
-    // @ts-ignore
-    if(canExecute(this.name, msg.author.id)) {
-      // @ts-ignore
-      setCooldown(this.name,msg.author.id, this.cooldown)
-      const res = await axios.get(`https://api.waifu.pics/sfw/waifu`);
-      const data: WaifuResponse = res.data;
-      await msg.reply({
-        embeds: [new MessageEmbed().setImage(data.url).setColor("#ff00ff")],
-      });
-    }
+    name: "waifu",
+    description: "Sends a random waifu image",
+    cooldown: 200,
+    execute: async (msg, _args) => {
 
-  },
+        const res = await axios.get(`https://api.waifu.pics/sfw/waifu`);
+        const data: WaifuResponse = res.data;
+        await msg.reply({
+            embeds: [new MessageEmbed().setImage(data.url).setColor("#ff00ff")],
+        });
+
+
+    },
 };
 
 module.exports = cmd;
