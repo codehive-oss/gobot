ded6ca52979c5f9718d1e9758096f81986322315
Duckulus <duckelite1@gmail.com>
2021-11-07 20:04:02 +0100

    waifu

--- src/commands/waifu.ts
+++ src/commands/waifu.ts
@@ -6,19 +6,19 @@ interface WaifuResponse {
     url: string;
 }
 
-//TODO add cooldown
+
 const cmd: CooldownCommand = {
     name: "waifu",
     description: "Sends a random waifu image",
     cooldown: 200,
     execute: async (msg, _args) => {
-
-        const res = await axios.get(`https://api.waifu.pics/sfw/waifu`);
-        const data: WaifuResponse = res.data;
-        await msg.reply({
-            embeds: [new MessageEmbed().setImage(data.url).setColor("#ff00ff")],
-        });
-
+        await msg.reply("Anime is a crime punishable by death")
+        // const res = await axios.get(`https://api.waifu.pics/sfw/waifu`);
+        // const data: WaifuResponse = res.data;
+        // await msg.reply({
+        //     embeds: [new MessageEmbed().setImage(data.url).setColor("#ff00ff")],
+        // });
+        //
 
     },
 };
