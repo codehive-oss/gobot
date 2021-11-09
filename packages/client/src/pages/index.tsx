a6197dc52990342fff73bf88b9a456639cad72b6
SushiWaUmai <eugene.bb1@gmail.com>
2021-11-09 18:15:30 +0100

    feat(client): :sparkles: add update landing home page

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import NavbarProvider from "../components/NavbarProvider";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <div>
       <NavbarProvider>
         <div className="py-5 flex justify-center">
+          <Image src="/GoBotTransparent.png" width={64} height={64} alt="GoBot" />
           <p className="text-6xl">GoBot</p>
         </div>
       </NavbarProvider>
