913fbcbc3ebd096572e1cee70647bcba3eb979e0
SushiWaUmai <eugene.bb1@gmail.com>
2021-11-12 19:08:03 +0100

    feat(frontend): :sparkles: add graphql client connection on frontend

---
+++ packages/frontend/src/generated/graphql.tsx
@@ -0,0 +1,84 @@
+import gql from 'graphql-tag';
+import * as Urql from 'urql';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Command = {
+  __typename?: 'Command';
+  aliases?: Maybe<Array<Scalars['String']>>;
+  category?: Maybe<Scalars['String']>;
+  description: Scalars['String'];
+  name: Scalars['String'];
+  usage?: Maybe<Scalars['String']>;
+};
+
+export type GoServer = {
+  __typename?: 'GoServer';
+  anime: Scalars['Boolean'];
+  id: Scalars['String'];
+  nsfw: Scalars['Boolean'];
+  prefix: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  setAnime: GoServer;
+  setNSFW: GoServer;
+  setPrefix: GoServer;
+};
+
+
+export type MutationSetAnimeArgs = {
+  anime: Scalars['Boolean'];
+  serverID: Scalars['String'];
+};
+
+
+export type MutationSetNsfwArgs = {
+  nsfw: Scalars['Boolean'];
+  serverID: Scalars['String'];
+};
+
+
+export type MutationSetPrefixArgs = {
+  prefix: Scalars['String'];
+  serverID: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  getCommands: Array<Command>;
+};
+
+export type GetCommandsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetCommandsQuery = { __typename?: 'Query', getCommands: Array<{ __typename?: 'Command', name: string, description: string, aliases?: Array<string> | null | undefined, usage?: string | null | undefined, category?: string | null | undefined }> };
+
+
+export const GetCommandsDocument = gql`
+    query GetCommands {
+  getCommands {
+    name
+    description
+    aliases
+    usage
+    category
+  }
+}
+    `;
+
+export function useGetCommandsQuery(options: Omit<Urql.UseQueryArgs<GetCommandsQueryVariables>, 'query'> = {}) {
+  return Urql.useQuery<GetCommandsQuery>({ query: GetCommandsDocument, ...options });
+};
--- packages/frontend/src/pages/vote.tsx
+++ packages/frontend/src/pages/vote.tsx
@@ -1,16 +1,17 @@
 import { NextPage } from "next";
 import NavbarProvider from "../components/NavbarProvider";
 
-interface VotePageProps {
-  
-}
- 
+interface VotePageProps {}
+
 const VotePage: NextPage<VotePageProps> = () => {
-  return <NavbarProvider>
-    <div>
-      <h1>Vote Page</h1>
-    </div>
-  </NavbarProvider>
-}
- 
+  return (
+    <NavbarProvider>
+      <div className="text-center">
+        <h1 className="text-5xl">Vote</h1>
+        <br />
+      </div>
+    </NavbarProvider>
+  );
+};
+
 export default VotePage;
