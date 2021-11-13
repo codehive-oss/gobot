163e152e8b080345cc086e715c10ca2e3be0e565
SushiWaUmai <eugene.bb1@gmail.com>
2021-11-13 10:52:46 +0100

    feat(backend): :sparkles: update dong
    Add a message when the dong is 69cm

--- packages/backend/src/commands/misc/dong.ts
+++ packages/backend/src/commands/misc/dong.ts
@@ -1,34 +1,33 @@
 import { Command } from "../../utils/commandTypes";
-import {Message, User} from "discord.js";
+import { Message } from "discord.js";
 
 const cmd: Command = {
-    aliases: ["dong"],
-    name: "dongsize",
-    description: "magnum schlong",
-    async execute(msg: Message, _args: string[]) {
-        const target = msg.mentions.users.first()
-        if(!target) {
-
-            await msg.reply(dong(msg.author))
-
-        }else {
-
-            await msg.reply(`${msg.author.username}'s Dong: \n ${dong(msg.author)}\n ${target.username}'s Dong: \n ${dong(target)}\n`)
-        }
-
+  aliases: ["dong"],
+  name: "dongsize",
+  description: "magnum schlong",
+  async execute(msg: Message, _args: string[]) {
+    const target = msg.mentions.users.first();
+    if (!target) {
+      await msg.reply(dong());
+    } else {
+      await msg.reply(
+        `${msg.author.username}'s Dong: \n ${dong()}\n ${
+          target.username
+        }'s Dong: \n ${dong()}\n`
+      );
     }
+  },
 };
 
-function dong(user: User) : string {
-    let random = Math.round(Math.random() * 100);
-    if(user.id==="347130390181445633") {
-        random = 100
-    }
-    let dong = "8";
-    for (let i = 0; i < random; i++) {
-        dong += "=";
-    }
-    return dong + "D " + random + "cm"
+function dong(): string {
+  let random = Math.round(Math.random() * 100);
+  let dong = "8";
+  for (let i = 0; i < random; i++) {
+    dong += "=";
+  }
+
+  // Add a message when the dong is 69cm
+  return dong + "D " + random + "cm" + (random === 69 ? "\nNOICE" : "");
 }
 
 module.exports = cmd;
