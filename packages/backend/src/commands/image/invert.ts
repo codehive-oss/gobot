508c4cdf1199e937e79918e7aa874ebd1c0f9e40
Duckulus <duckelite1@gmail.com>
2021-11-13 08:57:36 +0100

    feat(command): added invert command

---
+++ packages/backend/src/commands/image/invert.ts
@@ -0,0 +1,37 @@
+import {Command} from "../../utils/commandTypes";
+import {Message} from "discord.js";
+import Jimp from "jimp";
+import jimp from "jimp";
+import logger from "../../utils/logger";
+import {getTarget} from "../../utils/getTarget";
+
+const cmd: Command = {
+    name: "invert",
+    description: "Inverts the given User",
+    usage: "invert <@user>",
+    category: "image",
+    async execute(msg: Message, _args: string[]) {
+        const target = getTarget(msg)
+
+        if (!target!.avatarURL()) {
+            await msg.reply("That User does not have a Profile Picture");
+            return;
+        }
+
+        const image = await Jimp.read(target!.displayAvatarURL({format: "png", size:256}))
+        image.invert()
+
+        image.getBuffer(jimp.MIME_PNG, async (err, buffer) => {
+            if (err) {
+                logger.error(err);
+            }
+
+            await msg.reply({
+                files: [{attachment: buffer, name: "invert.png"}],
+            });
+        });
+
+    },
+};
+
+module.exports = cmd;
