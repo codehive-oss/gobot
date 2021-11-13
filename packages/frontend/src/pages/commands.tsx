cf453e1853901249f5dd3c12771a1cfd9ca71fbc
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-11-13 19:15:06 +0100

    feat(frontend): :sparkles: add command list in frontend

--- packages/frontend/src/pages/commands.tsx
+++ packages/frontend/src/pages/commands.tsx
@@ -1,28 +1,52 @@
 // import { useState } from "react";
 import { NextPage } from "next";
+import { useState } from "react";
+import CommnadsListComponent from "../components/CommandsList";
 import NavbarProvider from "../components/NavbarProvider";
-import { useGetCommandsQuery } from "../generated/graphql";
+import { useGetCategoriesQuery } from "../generated/graphql";
 
 interface CommandsPageProps {}
 
 const CommandsPage: NextPage<CommandsPageProps> = () => {
-  const commands = useGetCommandsQuery()[0];
+  const categories = useGetCategoriesQuery()[0];
+  const [selectedCategory, setSelectedCategory] = useState<string>();
 
   return (
     <NavbarProvider>
       <div className="text-center">
         <h1 className="text-5xl">Commands</h1>
         <br />
-        <ul className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {commands.data?.getCommands.map((command, i) => {
-            return (
-              <li className="p-4" key={i}>
-                <h2 className="text-2xl">{command.name}</h2>
-                <p>{command.description}</p>
-              </li>
-            );
-          })}
-        </ul>
+        {selectedCategory ? (
+          <>
+            <CommnadsListComponent category={selectedCategory} />
+            {/* Create a button that resets the selected category */}
+
+            <button
+              className="bg-gray-800 py-2 px-4 rounded"
+              type="button"
+              onClick={() => setSelectedCategory(undefined)}
+            >
+              All
+            </button>
+          </>
+        ) : (
+          <div>
+            <h2 className="text-2xl">Categories</h2>
+            <div className="flex flex-wrap justify-center">
+              {categories.data?.getCategories.map((category, i) => (
+                <div
+                  key={i}
+                  className="w-1/3 p-2 cursor-pointer"
+                  onClick={() => setSelectedCategory(category)}
+                >
+                  <div className="bg-gray-800 rounded-lg shadow-md p-2">
+                    <h3 className="text-xl">{category}</h3>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </NavbarProvider>
   );
