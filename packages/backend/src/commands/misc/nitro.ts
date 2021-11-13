d64c1eb91fc27497fb98f7ef22a8e61b94b74f51
Duckulus <duckelite1@gmail.com>
2021-11-13 16:53:23 +0100

    feat(command): added nitro command

---
+++ packages/backend/src/commands/misc/nitro.ts
@@ -0,0 +1,15 @@
+import {Command} from "../../utils/commandTypes";
+import {Message} from "discord.js";
+import {randString} from "../../utils/random";
+
+const cmd: Command = {
+    name: "nitro",
+    description: "Sends a random nitro gift code",
+    usage: "nitro",
+    async execute(msg: Message, _args: string[]) {
+        await msg.reply(`https://discord.gift/${randString(16)}`)
+
+    },
+};
+
+module.exports = cmd;
--- packages/backend/src/utils/random.ts
+++ packages/backend/src/utils/random.ts
@@ -5,3 +5,14 @@ export function randInt(min: number, max: number) {
 export function randomChoice(arr: any[]) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
+
+export function randString(length: number) {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
