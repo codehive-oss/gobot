c63d410e5cfda842333ed7da5fb9b53113b12d64
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-11-23 21:31:52 +0100

    revert(command): :rewind: reverted anime command

--- packages/backend/src/commands/misc/anime.ts
+++ packages/backend/src/commands/misc/anime.ts
@@ -1,12 +1,62 @@
+import axios from "axios";
+import { MessageEmbed } from "discord.js";
 import { Command } from "../../utils/commandTypes";
-import {Message} from "discord.js";
 
-const cmd : Command = {
-    name: "anime",
-    description: "anime",
-    async execute(msg: Message, _args: string[]) {
-        await msg.reply("anime is a crime punishable by death")
-    }
+interface AnimeResponse {
+  url: string;
 }
 
-module.exports = cmd
+const AnimeCategories = [
+  "waifu",
+  "neko",
+  "shinobu",
+  "megumin",
+  "bully",
+  "cuddle",
+  "cry",
+  "hug",
+  "awoo",
+  "kiss",
+  "lick",
+  "pat",
+  "smug",
+  "bonk",
+  "yeet",
+  "blush",
+  "smile",
+  "wave",
+  "highfive",
+  "handhold",
+  "nom",
+  "bite",
+  "glomp",
+  "slap",
+  "kill",
+  "kick",
+  "happy",
+  "wink",
+  "poke",
+  "dance",
+  "cringe",
+];
+
+// TODO: Add check for anime category
+const cmd: Command = {
+  name: "anime",
+  description: "Sends a random anime picture",
+  execute: async (msg, args) => {
+    let category = "waifu";
+    if (args[0]) {
+      if (AnimeCategories.includes(args[0].toLowerCase())) {
+        category = args[0].toLowerCase();
+      }
+    }
+    const res = await axios.get(`https://api.waifu.pics/sfw/${category}`);
+    const data: AnimeResponse = res.data;
+    await msg.reply({
+      embeds: [new MessageEmbed().setImage(data.url).setColor("#ff00ff")],
+    });
+  },
+};
+
+module.exports = cmd;
