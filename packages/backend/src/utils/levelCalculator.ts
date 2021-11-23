dca72ca780407ceac12c3bf281db3cb2d8652b29
Duckulus <duckelite1@gmail.com>
2021-11-23 14:56:32 +0100

    feat(command): added level and progressbar to xp system

---
+++ packages/backend/src/utils/levelCalculator.ts
@@ -0,0 +1,10 @@
+const x = 0.3
+const y = 2
+
+export function xpToLvl(xp : number) : number {
+    return Math.floor(x * Math.sqrt(xp))
+}
+
+export function lvlToXp(lvl : number) : number {
+    return Math.floor(Math.pow((lvl/x),2))
+}
