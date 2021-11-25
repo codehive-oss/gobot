781b454a115e2f253525335a70fd2ee4688f702a
Duckulus <duckelite1@gmail.com>
2021-11-25 16:45:57 +0100

    feat(command): added avatar command

---
+++ packages/backend/src/commands/image/avatar.ts
@@ -0,0 +1,26 @@
+import {Command} from "../../utils/commandTypes";
+import {randInt} from "../../utils/random";
+import {getTarget} from "../../utils/getTarget";
+
+const cmd: Command = {
+    name: "avatar",
+    description: "Sends a Users Avatar",
+    usage: "avatar <@user>",
+    aliases: ["av"],
+    category: "image",
+    execute: async (msg, args) => {
+        if (randInt(0, 100) == 50) {
+            await msg.reply("Der Herr der Elemente")
+            return
+        }
+
+        const target = getTarget(msg)
+        const avatar = target.displayAvatarURL({size: 1024, dynamic: true})
+
+        await msg.reply(avatar)
+
+
+    }
+}
+
+module.exports = cmd
