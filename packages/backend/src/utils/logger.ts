f8b44d78058d847ff6c195e24277a4b3795f0873
SushiWaUmai <eugene.bb1@gmail.com>
2021-11-28 13:52:12 +0100

    feat(backend): :loud_sound: update logging libraries

--- packages/backend/src/utils/logger.ts
+++ packages/backend/src/utils/logger.ts
@@ -1,20 +1,33 @@
 import pino from "pino";
 import expressPinoLogger from "express-pino-logger";
+import pinoPretty from "pino-pretty";
+import { buildMode, __prod__ } from "./constants";
+import fs from "fs";
 
-export const logger = pino({
-  prettyPrint: {
-    colorize: true,
-    translateTime: "SYS:standard",
-    messageFormat: (log) => {
-      if (log.req)
-        return `${log.req.method} ${log.req.url}: ${log.responseTime}ms - [${log.res.statusCode}] ${log.msg}`;
-      return `${log.msg}`;
-    },
-    hideObject: true,
+const prettifier = pinoPretty({
+  colorize: true,
+  translateTime: "SYS:standard",
+  hideObject: true,
+  messageFormat: (log: pino.LogDescriptor) => {
+    if (log.req) {
+      return `${log.req.method} ${log.req.url}: ${log.responseTime}ms - [${log.res.statusCode}] ${log.msg}`;
+    }
+    return `${log.msg}`;
   },
-  level: "trace",
-});
+}) as any;
+
+const logsDir = `./logs`;
+if (__prod__ && !fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
+}
+
+export const logger = pino(
+  {
+    level: "trace",
+  },
+  !__prod__
+    ? prettifier
+    : pino.destination(`${logsDir}/${Date.now()}-${buildMode}.log`)
+);
 
 export const expressLogger = expressPinoLogger({ logger });
-
-
