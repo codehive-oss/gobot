2b864c8d84ef94abec6255dc1c231445bd1b1e8b
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-12-12 12:44:08 +0100

    feat(frontend): :sparkles: setup cache and ssr

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -2,12 +2,10 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import LayoutComponent from "../components/LayoutComponent";
 import Head from "next/head";
-import { createClient, Provider } from "urql";
-import { backendUrl, __prod__ } from "../utils/constants";
+import { Provider } from "urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-const client = createClient({
-  url: `${backendUrl}/graphql`,
-});
+const client = createUrqlClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
