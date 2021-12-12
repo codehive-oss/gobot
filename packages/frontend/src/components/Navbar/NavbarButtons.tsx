44f76dd5d195886fc98a80c79bf8282661046a7e
Duckulus <duckelite1@gmail.com>
2021-12-12 11:24:52 +0100

    refactor(frontend): added bot invite to navbar

--- packages/frontend/src/components/Navbar/NavbarButtons.tsx
+++ packages/frontend/src/components/Navbar/NavbarButtons.tsx
@@ -1,10 +1,14 @@
 import LinkComponent from "../LinkComponent";
+import ExternalLinkComponent from "../ExternalLinkComponent";
 
 type NavbarButtonsProps = React.HTMLAttributes<HTMLDivElement>;
 
 const NavbarButtons: React.FC<NavbarButtonsProps> = ({ className }) => {
   return (
     <>
+        <ExternalLinkComponent className={className} href={"https://discord.com/oauth2/authorize?client_id=907449509133561888&permissions=8&scope=bot%20applications.commands"}>
+            Invite
+        </ExternalLinkComponent>
       <LinkComponent className={className} href="/commands">
         Commands
       </LinkComponent>
