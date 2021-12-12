d0dce468a003922188e1a4e7130450b7a5047b56
Duckulus <duckelite1@gmail.com>
2021-12-12 16:45:48 +0100

    feat: :sparkles: Added message leaderboard

--- packages/backend/src/commands/level/leaderboard.ts
+++ packages/backend/src/commands/level/leaderboard.ts
@@ -1,33 +1,123 @@
-import { Command } from "../../utils/commandTypes";
-import { Message, MessageEmbed } from "discord.js";
+import {
+  Command,
+  CommandSelectMenuInteraction,
+} from "../../utils/commandTypes";
+import {
+  Message,
+  MessageActionRow,
+  MessageEmbed,
+  MessageSelectMenu,
+  SelectMenuInteraction,
+} from "discord.js";
 import { GoUser } from "../../db/entities/GoUser";
 import { client } from "../../utils/client";
 import { mention } from "../../utils/mention";
+import { MissingSubscriptionTopicsError } from "type-graphql";
+import { emit } from "process";
 
-const cmd: Command = {
+const cmd: Command & CommandSelectMenuInteraction = {
   name: "leaderboard",
   category: "level",
   usage: "leaderboard",
   description: "Shows the Users with the Most Experience",
-  async execute(msg: Message, _args: string[]) {
-    const embed = new MessageEmbed()
-      .setTitle("Leaderboard")
-      .setColor("BLURPLE");
-
-    if (client.user?.avatarURL()) {
-      embed.setThumbnail(client.user.avatarURL() as string);
+  async execute(msg: Message, args: string[]) {
+    if (args.length > 0) {
+      switch (args[0].toLowerCase()) {
+        case "xp":
+          await msg.reply({
+            embeds: [await getXpLeaderboard()],
+          });
+          break;
+        case "msg":
+          await msg.reply({
+            embeds: [await getMessageLeaderboard()],
+          });
+          break;
+        default:
+          break;
+      }
     }
-
-    let description = "Top 10 Users With The Most experience \n";
-    let position = 1;
-    for (const user of await GoUser.find({ take: 10, order: { xp: "DESC" } })) {
-      description += `\n  ${position}. ${mention(user.id)} |  ${user.xp}xp`;
-      position++;
+    const row = new MessageActionRow().addComponents(
+      new MessageSelectMenu()
+        .setCustomId(this.name)
+        .setPlaceholder("Category")
+        .addOptions([
+          {
+            label: "XP",
+            value: "xp",
+            description: "Leaderboard basen on Experience",
+          },
+          {
+            label: "Messages",
+            value: "msg",
+            description: "Leaderboard basen on Messages",
+          },
+        ])
+    );
+    await msg.reply({
+      content: "Select Which Leaderboard you would like to view",
+      components: [row],
+    });
+  },
+  handleInteraction: async (interaction: SelectMenuInteraction) => {
+    switch (interaction.values[0]) {
+      case "xp":
+        await interaction.reply({
+          content: mention(interaction.user.id),
+          embeds: [await getXpLeaderboard()],
+        });
+        break;
+      case "msg":
+        await interaction.reply({
+          content: mention(interaction.user.id),
+          embeds: [await getMessageLeaderboard()],
+        });
+        break;
+      default:
+        await interaction.reply("Error");
     }
-
-    embed.setDescription(description);
-    await msg.reply({ embeds: [embed] });
   },
 };
+
+async function getXpLeaderboard() {
+  const embed = new MessageEmbed().setTitle("Leaderboard").setColor("BLURPLE");
+
+  if (client.user?.avatarURL()) {
+    embed.setThumbnail(client.user.avatarURL() as string);
+  }
+
+  let description = "Top 10 Users With The Most experience \n";
+  let position = 1;
+  for (const user of await GoUser.find({ take: 10, order: { xp: "DESC" } })) {
+    description += `\n  ${position}. ${mention(user.id)} |  ${user.xp}xp`;
+    position++;
+  }
+
+  embed.setDescription(description);
+  return embed;
+}
+
+async function getMessageLeaderboard() {
+  const embed = new MessageEmbed().setTitle("Leaderboard").setColor("BLURPLE");
+
+  if (client.user?.avatarURL()) {
+    embed.setThumbnail(client.user.avatarURL() as string);
+  }
+
+  let description = "Top 10 Users With The Most Messages \n";
+  let position = 1;
+  for (const user of await GoUser.find({
+    take: 10,
+    order: { messages: "DESC" },
+  })) {
+    description += `\n  ${position}. ${mention(user.id)} |  ${
+      user.messages
+    } Messages`;
+    position++;
+  }
+
+  embed.setDescription(description);
+  return embed;
+}
 
 module.exports = cmd;
--- packages/frontend/src/components/ExternalLinkComponent.tsx
+++ packages/frontend/src/components/ExternalLinkComponent.tsx
@@ -12,7 +12,7 @@ const ExtenalLinkComponent: React.FC<ExternalLinkComponentProps> = ({
     return (
         <div className={className}>
 
-            <a href={href} target="_blank">{children}</a>
+            <a href={href} target="_blank" rel="noreferrer">{children}</a>
         </div>
     );
 };
