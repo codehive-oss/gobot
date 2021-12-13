00e13aeabacd49fdf7d556d3feabfc3c1e18df04
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-12-13 16:38:04 +0100

    feat(framer-motion): :sparkles: add framer motion and react-intersection-observer

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import NavbarProvider from "../components/NavbarProvider";
 import Image from "next/image";
 import Head from "next/head";
+import FadeProvider from "../components/animated/FadeProvider";
 
 const Home: NextPage = () => {
   return (
@@ -10,18 +11,20 @@ const Home: NextPage = () => {
         <title>Home | GoBot</title>
       </Head>
 
-      <div className="grid grid-cols-1 gap-4">
-        <div className="flex justify-center">
-          <Image
-            src="/GoBotTransparent.png"
-            width={256}
-            height={256}
-            alt="GoBot"
-          />
+      <FadeProvider>
+        <div className="grid grid-cols-1 gap-4">
+          <div className="flex justify-center">
+            <Image
+              src="/GoBotTransparent.png"
+              width={256}
+              height={256}
+              alt="GoBot"
+            />
+          </div>
+          <div className="font-mono text-center text-6xl">GoBot</div>
+          <div className="font-mono text-center text-2xl">DISCORD BOT</div>
         </div>
-        <div className="font-mono text-center text-6xl">GoBot</div>
-        <div className="font-mono text-center text-2xl">DISCORD BOT</div>
-      </div>
+      </FadeProvider>
     </NavbarProvider>
   );
 };
